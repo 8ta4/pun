840da397bf7b5203a58d2b57ed12a0a6653fb6d2
a <a@as-Virtual-Machine.local>
2025-04-01 23:38:10 -0700

    Enable nixpkgs-fmt, prettier, and trailing-whitespace git hooks

--- devenv.nix
+++ devenv.nix
@@ -40,6 +40,17 @@
 
   # https://devenv.sh/git-hooks/
   # git-hooks.hooks.shellcheck.enable = true;
+  git-hooks.hooks = {
+    nixpkgs-fmt.enable = true;
+    prettier.enable = true;
+    # https://github.com/cachix/git-hooks.nix/issues/31#issuecomment-744657870
+    trailing-whitespace = {
+      enable = true;
+      # https://github.com/pre-commit/pre-commit-hooks/blob/6db05e22aa7546f11ebde806dbf6fbf5985de07c/.pre-commit-hooks.yaml#L205-L212
+      entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/trailing-whitespace-fixer";
+      types = [ "text" ];
+    };
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
