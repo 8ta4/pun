deafb8906bc50ee398d5ab733b246ec6f482431b
a <a@as-Virtual-Machine.local>
2025-04-03 01:13:40 -0700

    Implement load-wiktextract-data function

--- src/core.clj
+++ src/core.clj
@@ -1,7 +1,21 @@
-(ns core)
+(ns core
+  (:require
+   [clojure.java.io :as io]
+   [cheshire.core :refer [parse-string]]))
 
 (def wiktextract-data-path
   (str (System/getProperty "user.home") "/.cache/pun/raw-wiktextract-data.jsonl.gz"))
+
+(defn load-wiktextract-data
+  []
+  (->> wiktextract-data-path
+       io/input-stream
+       java.util.zip.GZIPInputStream.
+       java.io.InputStreamReader.
+       java.io.BufferedReader.
+       line-seq
+       (map #(parse-string % keyword))
+       (filter (comp (partial = "English") :lang))))
 
 (defn -main
   "The main entry point for the application"
