cae0139a4e6ff74d60bb65669d7f15e3ec2332d4
a <a@as-Virtual-Machine.local>
2025-04-23 01:45:12 -0700

    Load phrase scores and IPA data into server namespace

--- src/server.clj
+++ src/server.clj
@@ -1,2 +1,10 @@
 (ns server
-  (:require [core :refer [normalized-path ipa-path]]))
+  (:require
+   [clojure.edn :as edn]
+   [core :refer [ipa-path normalized-path]]))
+
+(def phrase-scores
+  (edn/read-string (slurp normalized-path)))
+
+(def phrase-ipas
+  (edn/read-string (slurp ipa-path)))
