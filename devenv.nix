b47973acd784bb5d3728eea87459230a9939541e
a <a@as-Virtual-Machine.local>
2025-05-04 00:43:42 -0700

    Wrap gitleaks hook entry in bash execution

--- devenv.nix
+++ devenv.nix
@@ -60,8 +60,9 @@
     cljfmt.enable = true;
     gitleaks = {
       enable = true;
-      # https://github.com/gitleaks/gitleaks/blob/39947b0b0d3f1829438000819c1ba9dbeb023a89/.pre-commit-hooks.yaml#L4
-      entry = "gitleaks protect --verbose --redact --staged";
+      # https://github.com/gitleaks/gitleaks/blob/6f967cad68d7ce015f45f4545dca2ec27c34e906/.pre-commit-hooks.yaml#L4
+      # Direct execution of gitleaks here results in '[git] fatal: cannot change to 'devenv.nix': Not a directory'.
+      entry = "bash -c 'exec gitleaks git --redact --staged --verbose'";
     };
     # https://github.com/NixOS/nixfmt/blob/1acdae8b49c1c5d7f22fed7398d7f6f3dbce4c8a/README.md?plain=1#L16
     nixfmt-rfc-style.enable = true;
