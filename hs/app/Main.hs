ea74846b679d1d69a30af62bb70150826d23c509
a <a@as-Virtual-Machine.local>
2025-12-24 01:44:27 -0800

    Implement pun generation by calling a local HTTP service

--- hs/app/Main.hs
+++ hs/app/Main.hs
@@ -1,9 +1,15 @@
 module Main (main) where
 
 import Data.Text.IO (getContents)
+import Network.HTTP.Client.Conduit (parseRequest_)
+import Network.HTTP.Simple (getResponseBody, httpJSON, setRequestBodyJSON, setRequestMethod)
 import Relude
+
+generatePuns :: [Text] -> IO [Text]
+generatePuns targets = getResponseBody <$> (httpJSON $ setRequestBodyJSON targets $ setRequestMethod "POST" $ parseRequest_ "http://localhost:3000")
 
 main :: IO ()
 main = do
   input <- getContents
-  print $ lines input
+  puns <- generatePuns (lines input)
+  traverse_ putTextLn puns
