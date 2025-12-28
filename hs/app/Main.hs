5501c1c8730c44619bb6f02893cd39452359a843
a <a@as-Virtual-Machine.local>
2025-12-27 16:11:52 -0800

    Implement server startup logic in Main.hs

--- hs/app/Main.hs
+++ hs/app/Main.hs
@@ -3,10 +3,17 @@ module Main (main) where
 import Data.Text.IO (getContents)
 import Network.HTTP.Client.Conduit (parseRequest_)
 import Network.HTTP.Simple (getResponseBody, httpJSON, setRequestBodyJSON, setRequestMethod)
-import Relude
+import Relude hiding (lookupEnv)
+import System.Environment (lookupEnv)
+import System.Process (CreateProcess (cwd), createProcess, proc)
 
 generatePuns :: [Text] -> IO [Text]
 generatePuns targets = getResponseBody <$> (httpJSON $ setRequestBodyJSON targets $ setRequestMethod "POST" $ parseRequest_ "http://localhost:3000")
+
+startServer :: IO ()
+startServer = do
+  maybeRoot <- lookupEnv "DEVENV_ROOT"
+  void $ createProcess (proc "clj" ["-M", "-m", "server"]) {cwd = (<> "/clj") <$> maybeRoot}
 
 main :: IO ()
 main = do
